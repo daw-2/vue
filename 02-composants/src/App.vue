5952d8500e25c4c6730e7d4fb05e8540410af4d8
Matthieu Mota <matthieumota@gmail.com>
2023-11-30 11:06:51 +0100

    Vite and components

---
+++ 02-composants/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import { ref } from 'vue';
+  import Navbar from './components/Navbar.vue';
+
+  const title = ref('Mon Application');
+  const changeTitle = (newTitle) => title.value = newTitle;
+  const color = ref('#00bd7e33');
+</script>
+
+<template>
+  <header>
+    <h1 @click="changeTitle('Ma superbe Application')">{{ title }}</h1>
+
+    <Navbar :color="color" />
+    <input type="color" v-model="color">
+  </header>
+</template>
+
+<style scoped></style>
---
+++ 02-composants/src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const props = defineProps(['color']);
+
+  const active = ref('Accueil');
+  const refColor = ref(props.color); // Si on a besoin de modifier la valeur de la props
+</script>
+
+<template>
+  <nav :style="{ backgroundColor: color }">
+    <a href="#" @click.prevent="active = 'Accueil'" :class="{ active: active === 'Accueil' }">Accueil</a>
+    <a href="#" @click.prevent="active = 'Liste'" :class="{ active: active === 'Liste' }">Liste</a>
+    <a href="#" @click.prevent="active = 'A propos'" :class="{ active: active === 'A propos' }">A propos</a>
+  </nav>
+</template>
+
+<style scoped>
+  .active {
+    background-color: #00bd7e33;
+  }
+</style>
