21604522c2ebacd4f4864b34e17f2efed36f1c97
Matthieu Mota <matthieumota@gmail.com>
2023-12-01 10:48:18 +0100

    API

--- 03-router/src/views/HomeView.vue
+++ 03-router/src/views/HomeView.vue
@@ -1,6 +1,21 @@
 <script setup>
+  import axios from 'axios';
+  import { onMounted, ref } from 'vue';
+
+  const todos = ref([]);
+
+  onMounted(() => {
+    axios.get('http://localhost:3000/todos').then(response => {
+      todos.value = response.data;
+    });
+  });
 </script>
 
 <template>
-  Accueil
+  <div>
+    <div v-for="todo in todos" :key="todo.id">
+      {{ todo.name }}
+      {{ todo.done ? '✅' : '❌' }}
+    </div>
+  </div>
 </template>
