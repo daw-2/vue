240cfedc1079019bfaa882751f808a8046b65d74
Matthieu Mota <matthieumota@gmail.com>
2024-06-14 10:23:35 +0200

    Some exos

---
+++ exercices/09-new-year-countdown/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import CountdownHeader from "./components/CountdownHeader.vue";
+import CountdownSegment from "./components/CountdownSegment.vue";
+</script>
+<template>
+  <div class="app-wrapper">
+    <div class="countdown-box">
+      <CountdownHeader />
+      <main class="flex justify-center">
+        <CountdownSegment data-test="days" label="days" />
+        <CountdownSegment data-test="hours" label="hours" />
+        <CountdownSegment data-test="minutes" label="minutes" />
+        <CountdownSegment data-test="seconds" label="seconds" />
+      </main>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.app-wrapper {
+  @apply flex items-center justify-center w-full h-full p-10;
+}
+.countdown-box {
+  @apply shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px];
+}
+body {
+  @apply bg-gray-100;
+}
+</style>
